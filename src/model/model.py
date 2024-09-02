1c387423b8b228b096acaf4279c1c0ffe9b196ef
ortem917 <sarg.22@uni-dubna.ru>
2024-09-02 11:46:51 +0500

    add relationship in src.model.model

--- src/model/model.py
+++ src/model/model.py
@@ -1,21 +1,11 @@
 from fastapi_users_db_sqlalchemy import SQLAlchemyBaseUserTable
-from sqlalchemy import ForeignKey
+from sqlalchemy import ForeignKey, relationship
 
 from src.templates.database import str_50, id_primary, create_at, update_at, str_255
 from sqlalchemy.orm import Mapped, mapped_column, DeclarativeBase
 
 class Base(DeclarativeBase):
     pass
-
-class Note(Base):
-    __tablename__ = 'note'
-
-    id: Mapped[id_primary]
-    title: Mapped[str_50]
-    content: Mapped[str]
-    create_at: Mapped[create_at]
-    update_at: Mapped[update_at]
-    author_id: Mapped[int] = mapped_column(ForeignKey('user.id'))
 
 class User(SQLAlchemyBaseUserTable[int], Base):
     id: Mapped[id_primary]
@@ -27,3 +17,17 @@ class User(SQLAlchemyBaseUserTable[int], Base):
     is_active: Mapped[bool]
     is_superuser: Mapped[bool]
     is_verified: Mapped[bool]
+
+    notes: Mapped[list["Note"]] = relationship("Note", back_populates="author")
+
+class Note(Base):
+    __tablename__ = 'note'
+
+    id: Mapped[id_primary]
+    title: Mapped[str_50]
+    content: Mapped[str]
+    create_at: Mapped[create_at]
+    update_at: Mapped[update_at]
+    author_id: Mapped[int] = mapped_column(ForeignKey('user.id', ondelete='CASCADE'))
+
+    author: Mapped["User"] = relationship("User", back_populates="note")
